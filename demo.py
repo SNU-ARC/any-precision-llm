0012d38b58c353114344550cdb2abc38d371cfea
bg.sim <bg.sim@snu.ac.kr>
2024-02-20 15:14:59 +0900

    something wrong?

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 if __name__ == '__main__':
     o_model_path = 'facebook/opt-1.3b'
     q_model_path = 'cache/models/opt-1.3b.pt'
-    supported_bits = [4, 6]
+    supported_bits = [3, 4]
 
     tokenizer = AutoTokenizer.from_pretrained(o_model_path)
     config = AutoConfig.from_pretrained(o_model_path, trust_remote_code=True)
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # TODO : Why the model is already in GPU?
     model = model.eval().cuda()
 
-    input_context = "Yellow cat"
+    input_context = "I'm cat person, "
     input_ids = tokenizer.encode(input_context, return_tensors="pt")
     output = model.generate(input_ids.cuda(), max_length=256)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
