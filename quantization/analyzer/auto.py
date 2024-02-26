030eb582663b3f325ab87a7764e42ee2b92d8a69
Jake Hyun <jake.hyun@hotmail.com>
2024-02-26 18:37:17 +0900

    More fixes

--- quantization/analyzer/auto.py
+++ quantization/analyzer/auto.py
@@ -14,6 +14,7 @@ class AutoAnalyzer(ModelAnalyzer):
         for name, module in layers.named_modules():
             if isinstance(module, torch.nn.Linear):
                 module_names.append(name)
+        return module_names
 
     def get_model_name(self):
         return "model"  # TODO: implement this
@@ -21,7 +22,6 @@ class AutoAnalyzer(ModelAnalyzer):
     def get_layers_name(self):
         # Find attribute that ends with DecoderLayer
         model = self.get_model()
-        # Find class that ends with DecoderLayer
         for name, module in model.named_children():
             if isinstance(module, torch.nn.ModuleList):
                 return name
