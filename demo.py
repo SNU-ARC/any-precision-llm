43865d535829a1b7044de55bebf4e1b315ab4070
Jake Hyun <jake.hyun@hotmail.com>
2024-04-20 18:58:32 +0900

    Show latency in demo

--- demo.py
+++ demo.py
@@ -6,7 +6,6 @@ import time
 # Logging with time sans date, level name, and message
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s | %(levelname)s] %(message)s', datefmt='%H:%M:%S')
 
-
 if __name__ == '__main__':
     model_path = './cache/packed/anyprec-(Llama-2-7b-hf)-w8_orig3-gc1-c4_s100_blk512'
 
@@ -19,13 +18,25 @@ if __name__ == '__main__':
     input_context = "Large Language Models are"
     input_ids = tokenizer.encode(input_context, return_tensors="pt").cuda()
 
+    results = {}
+
     for precision in model.precisions:
         print(f"=============== generation with {precision}-bit precision ===============")
         start_time = time.time()
         output = model.generate(input_ids, precision=precision, max_length=100, pad_token_id=tokenizer.eos_token_id,
                                 streamer=streamer)
         end_time = time.time()
-        # print the generation speed
+
+        # Calculate generation speed
         token_count = len(output[0])
-        print(f"[[ Generation speed: {token_count / (end_time - start_time):.1f} tokens per second ]")
-        print(f"[[ Latency per token: {(end_time - start_time) / token_count * 1000:.2f} ms ]]")
+        tokens_per_second = token_count / (end_time - start_time)
+        ms_per_token = 1 / tokens_per_second * 1000
+
+        results[precision] = (tokens_per_second, ms_per_token)
+
+        print(f"\n( Generation speed: {tokens_per_second:.1f} tok/s | Latency: {ms_per_token:.2f} ms/tok )\n")
+
+    print("=============== Summary ===============")
+
+    for precision, (tokens_per_second, ms_per_token) in results.items():
+        print(f"{precision}-bit: {tokens_per_second:.1f} tok/s | {ms_per_token:.2f} ms/tok")
