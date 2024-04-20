7b1a7b45115ea79c73b9a1bd35258b735319c149
Jake Hyun <jake.hyun@hotmail.com>
2024-04-20 18:32:33 +0900

    Update text generation

--- demo.py
+++ demo.py
@@ -1,15 +1,18 @@
 from any_precision.modules import AnyPrecisionForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextStreamer
 import logging
+import time
 
 # Logging with time sans date, level name, and message
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s | %(levelname)s] %(message)s', datefmt='%H:%M:%S')
 
 
 if __name__ == '__main__':
-    model_path = './cache/packed/anyprec-(opt-1.3b)-w8_orig3-c4_s100_blk512'
+    model_path = './cache/packed/anyprec-(Llama-2-7b-hf)-w8_orig3-gc1-c4_s100_blk512'
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
+    streamer = TextStreamer(tokenizer)
+
     model = AnyPrecisionForCausalLM.from_quantized(model_path, precisions=[3, 4, 5, 6])
     model = model.eval().cuda()
 
@@ -18,6 +21,11 @@ if __name__ == '__main__':
 
     for precision in model.precisions:
         print(f"=============== generation with {precision}-bit precision ===============")
-        output = model.generate(input_ids, precision=precision, max_length=64, pad_token_id=tokenizer.eos_token_id)
-        output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-        print(output_text + '\n')
+        start_time = time.time()
+        output = model.generate(input_ids, precision=precision, max_length=64, pad_token_id=tokenizer.eos_token_id,
+                                streamer=streamer)
+        end_time = time.time()
+        # print the generation speed
+        token_count = len(output[0])
+        print(f"[[ Generation speed: {token_count / (end_time - start_time):.2f} tokens per second ]")
+        print(f"[[ Latency per token: {(end_time - start_time) / token_count * 1000:.2f} ms ]]")
