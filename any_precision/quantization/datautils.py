dc7c994cb837711b571ecb78ce3f38be9a3b8cf0
Jake Hyun <jake.hyun@hotmail.com>
2024-07-18 14:39:21 +0900

    Trust remote code and correct ptb <unk> handling

--- any_precision/quantization/datautils.py
+++ any_precision/quantization/datautils.py
@@ -7,18 +7,19 @@ import logging
 def _get_wikitext2(split):
     assert split in ['train', 'validation', 'test'], f"Unknown split {split} for wikitext2"
 
-    data = load_dataset('wikitext', 'wikitext-2-raw-v1', split=split)
+    data = load_dataset('wikitext', 'wikitext-2-raw-v1', split=split, trust_remote_code=True)
     return data['text']
 
 
 def _get_ptb(split, slice_unk=True):
     assert split in ['train', 'validation', 'test'], f"Unknown split {split} for ptb"
 
-    data = load_dataset('ptb_text_only', 'penn_treebank', split=split)
+    data = load_dataset('ptb_text_only', 'penn_treebank', split=split,
+                        trust_remote_code=True)
     data_list = data['sentence']
 
     if slice_unk:
-        data_list = [s.split() for s in data_list]
+        data_list = [s.replace('<unk>', '< u n k >') for s in data_list]
 
     return data_list
 
@@ -28,12 +29,14 @@ def _get_c4(split):
 
     if split == 'train':
         data = load_dataset(
-            'allenai/c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train'
+            'allenai/c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train',
+            trust_remote_code=True
         )
     else:
         assert split == 'validation'
         data = load_dataset(
-            'allenai/c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'}, split='validation'
+            'allenai/c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'}, split='validation',
+            trust_remote_code=True
         )
 
     return data['text']
@@ -42,7 +45,7 @@ def _get_c4(split):
 def _get_pileval(split):
     if split != 'validation':
         logging.warning(f"Pileval only has a validation split, but got split={split}. Using validation split.")
-    data = load_dataset("mit-han-lab/pile-val-backup", split="validation")
+    data = load_dataset("mit-han-lab/pile-val-backup", split="validation", trust_remote_code=True)
 
     return data['text']
 
