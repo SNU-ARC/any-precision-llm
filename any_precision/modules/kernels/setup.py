48c1c587887a8a84957a32d1c34ca069c433d039
Fuwen Tan <fuwen.tan@gmail.com>
2024-08-03 23:21:55 +0100

    cuda gencode

--- any_precision/modules/kernels/setup.py
+++ any_precision/modules/kernels/setup.py
@@ -1,11 +1,17 @@
+import torch
+
 from setuptools import setup
 from torch.utils import cpp_extension
+
+compute_capability = torch.cuda.get_device_capability()
+cuda_arch = compute_capability[0] * 100 + compute_capability[1] * 10
 
 setup(
     name="any_precision_ext",
     ext_modules=[
         cpp_extension.CUDAExtension(
-            "any_precision_ext", ["main.cu"]
+            "any_precision_ext", ["main.cu"],
+            extra_compile_args={'nvcc': [f'-DCUDA_ARCH={cuda_arch}']},
         )
     ],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
